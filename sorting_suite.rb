ed799a43257f969593be64b912293a4dc3f66469
Aaron Olsen <mraaronolsen@gmail.com>
2017-03-17 17:56:12 -0600

    Wrote Bubble Sort Pseudo

---
+++ sorting_suite.rb
@@ -0,0 +1,12 @@
+# Bubble Sort
+
+# loop through all elemements of collection (E1)
+  # taking E1 from E1 index loop by index through collection length (E2)
+    # if E1 > E2 swap elements
+
+# loop through all elements of "collection"
+  # starting at index i compare i to i+1
+    # if i > i+1 move i+1 to "temp_collection" and delete from "collection"
+    # if i < i+1 move i to "temp_collection" and delete from "collection"
+  # increment i
+# make collection temp_collection
