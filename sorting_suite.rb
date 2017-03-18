1543bef4e81075d2d0f99e73b37fdb0647776441
Aaron Olsen <mraaronolsen@gmail.com>
2017-03-17 18:44:42 -0600

    Made Bubble Sort Work

--- sorting_suite.rb
+++ sorting_suite.rb
@@ -1,12 +1,28 @@
+require 'pry'
 # Bubble Sort
 
-# loop through all elemements of collection (E1)
-  # taking E1 from E1 index loop by index through collection length (E2)
-    # if E1 > E2 swap elements
-
-# loop through all elements of "collection"
-  # starting at index i compare i to i+1
+# loop through all elements to end of "collection" until counter equals collection length
+  # starting at index i compare i to i+1 until length is 1
     # if i > i+1 move i+1 to "temp_collection" and delete from "collection"
     # if i < i+1 move i to "temp_collection" and delete from "collection"
-  # increment i
+  # move last element to beginning of sorted array
+  # increment counter
 # make collection temp_collection
+
+
+collection = []
+100.times { collection << rand(0..100) }
+sorted_collection = []
+
+until collection.empty?
+  temp = []
+  until collection.length == 1
+    collection[0] >= collection[1] ? temp << collection.slice!(1) : temp << collection.slice!(0)
+  end
+  sorted_collection.unshift(collection[0])
+  collection = temp
+end
+
+puts ""
+print sorted_collection
+puts ""
